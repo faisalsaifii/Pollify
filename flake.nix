8be4ba395b3b20fabd7b8ee536fbd6f9f9919087
faisalsaifi <m.faisal.s@outlook.com>
2023-11-17 00:21:14 +0530

    nix init

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Pollify Slack App";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default =
+          let
+            python = pkgs.python311;
+          in
+          pkgs.mkShell {
+            # The Nix packages provided in the environment
+            packages = [
+              # Python plus helper tools
+              (python.withPackages (ps: with ps; [
+                flask
+                slack-bolt
+                gunicorn
+                pymongo
+                uuid
+              ]))
+            ];
+
+            shellHook = ''
+              FLASK_APP=app.py python3 -m flask --debug run -p 5001
+            '';
+          };
+      });
+    };
+}
